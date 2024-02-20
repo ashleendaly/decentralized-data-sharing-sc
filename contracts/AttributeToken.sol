321bf7ca9b02203e29dbdc84aabacc8bf3cf7e39
ashleendaly <Ashleen.Daly@res-group.com>
2024-02-20 02:15:18 +0000

    smart contracts

---
+++ contracts/AttributeToken.sol
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
+
+contract AttributeToken is ERC1155, Ownable, ERC1155Burnable {
+    mapping(uint256 => address) public tokenCreators;
+
+    constructor(address initialOwner) ERC1155("") Ownable(initialOwner) {}
+
+    function setURI(string memory newuri) public onlyOwner {
+        _setURI(newuri);
+    }
+
+    function mint(
+        address account,
+        uint256 id,
+        uint256 amount,
+        bytes memory data
+    ) public onlyOwner {
+        _mint(account, id, amount, data);
+    }
+
+    function mintBatch(
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) public onlyOwner {
+        _mintBatch(to, ids, amounts, data);
+    }
+
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 id,
+        uint256 amount,
+        bytes memory data
+    ) public override {
+        require(
+            from == tokenCreators[id]
+        );
+        super.safeTransferFrom(from, to, id, amount, data);
+    }
+
+    function safeBatchTransferFrom(
+        address from,
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) public override {
+        for (uint256 i = 0; i < ids.length; ++i) {
+            require(
+                from == tokenCreators[ids[i]]
+            );
+        }
+        super.safeBatchTransferFrom(from, to, ids, amounts, data);
+    }
+
+    function callerIsTokenHolder(uint256 id) public view returns (bool) {
+        return balanceOf(msg.sender, id) > 0;
+    }
+}
---
+++ contracts/IPFSUploader.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+contract UploadAndRequestIPFS {
+    struct UploadMetadata {
+        uint uploadedAt;
+        string policyString;
+        bytes32 ipfsHash;
+    }
+
+    mapping(address => UploadMetadata) public uploads;
+
+    function upload(
+        bytes32 ipfsHash,
+        string memory policyString
+    ) external returns (UploadMetadata memory newUpload) {
+        newUpload.uploadedAt = block.timestamp;
+        newUpload.policyString = policyString;
+        newUpload.ipfsHash = ipfsHash;
+        uploads[msg.sender] = newUpload;
+        return newUpload;
+    }
+}
