c7aa4527a71171ba16f069bc05e81d36d69931a3
ashleendaly <Ashleen.Daly@res-group.com>
2024-03-11 18:26:59 +0000

    get all uploads for address

--- contracts/IPFSUploader.sol
+++ contracts/IPFSUploader.sol
@@ -1,18 +1,31 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.20;
 
 contract UploadIPFS {
-    struct UploadObject {
+    struct Upload {
         string policyString;
         string ipfsHash;
     }
 
-    mapping(address => UploadObject[]) public uploads;
+    mapping(address => Upload[]) public uploads;
 
-    function upload(string memory ipfsHash, string memory policyString) external returns (UploadObject memory newUpload) {
+    function upload(string memory ipfsHash, string memory policyString) external returns (Upload memory newUpload) {
         newUpload.policyString = policyString;
         newUpload.ipfsHash = ipfsHash;
         uploads[msg.sender].push(newUpload);
         return newUpload;
     }
+
+    function getAllUploadsForAddress(address user) external view returns (string[] memory policyStrings, string[] memory ipfsHashes) {
+        uint256 length = uploads[user].length;
+        policyStrings = new string[](length);
+        ipfsHashes = new string[](length);
+
+        for (uint256 i = 0; i < length; i++) {
+            Upload storage _upload = uploads[user][i];
+            policyStrings[i] = _upload.policyString;
+            ipfsHashes[i] = _upload.ipfsHash;
+        }
+    }
+
 }
