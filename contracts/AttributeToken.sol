8c077001a82ffd2f344fb93b1792589ddc0a4880
ashleendaly <Ashleen.Daly@res-group.com>
2024-03-26 10:14:43 +0000

    add security checks to burn functions

--- contracts/AttributeToken.sol
+++ contracts/AttributeToken.sol
@@ -50,6 +50,21 @@ contract AttributeToken is ERC1155, ERC1155Burnable {
         super.safeTransferFrom(from, to, id, amount, data);
     }
 
+    function safeBatchTransferFrom(
+        address from,
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) public override {
+        for (uint256 i = 0; i < ids.length; ++i) {
+            require(from == msg.sender, "Caller must be the token owner");
+            require(tokenCreators[ids[i]] == msg.sender, "Caller is not the creator of the token");
+            require(balanceOf(msg.sender, ids[i]) >= amounts[i], "Caller does not have enough tokens");
+        }
+        super.safeBatchTransferFrom(from, to, ids, amounts, data);
+    }
+
     function VerifySignedAddress(bytes32 _hashedMessage, uint8 _v, bytes32 _r, bytes32 _s) public pure returns (address) {
         bytes memory prefix = "\x19Ethereum Signed Message:\n32";
         bytes32 prefixedHashMessage = keccak256(abi.encodePacked(prefix, _hashedMessage));
