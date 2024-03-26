71205b9562dccbe814e3850687ae1bfe5931b59a
ashleendaly <Ashleen.Daly@res-group.com>
2024-03-26 17:03:00 +0000

    connected hardhat to network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,34 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+require("dotenv").config();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.20",
+  defaultNetwork: "sepolia",
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: process.env.SEPOLIA_URL,
+      accounts: [process.env.PRIVATE_KEY || ""],
+    },
+  },
+  solidity: {
+    version: "0.8.24",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
+  mocha: {
+    timeout: 40000,
+  },
 };
 
 export default config;
